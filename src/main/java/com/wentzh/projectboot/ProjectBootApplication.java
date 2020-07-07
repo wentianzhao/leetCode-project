2f2fe44afbea4754cf99c6c34aab4598178adab5
wentianzhao <wentian8340@gmail.com>
2020-07-07 17:34:07 +0800

    first commit

--- src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
+++ src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
@@ -2,14 +2,13 @@ package com.wentzh.projectboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @SpringBootApplication
 public class ProjectBootApplication {
 
     /*@RequestMapping("index")
+
     public String index(){
         return "hello world!";
     }*/
