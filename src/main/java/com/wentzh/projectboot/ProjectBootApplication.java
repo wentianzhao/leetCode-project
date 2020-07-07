1d5860df952293965cc3def85de9dd1d1bf391ee
wentianzhao <wentian8340@gmail.com>
2020-07-07 17:35:32 +0800

    second commit

--- src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
+++ src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectBootApplication {
 
     /*@RequestMapping("index")
-
     public String index(){
         return "hello world!";
     }*/
