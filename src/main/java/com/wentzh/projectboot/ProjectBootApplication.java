dc318899281d5e742f4d8fefa37b5fa32f3c7611
wentianzhao <wentian8340@gmail.com>
2020-07-08 10:42:58 +0800

    删除之前代码

--- src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
+++ src/main/java/com/wentzh/projectboot/ProjectBootApplication.java
@@ -3,14 +3,8 @@ package com.wentzh.projectboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@RestController
 @SpringBootApplication
 public class ProjectBootApplication {
-
-    /*@RequestMapping("index")
-    public String index(){
-        return "hello world!";
-    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectBootApplication.class, args);
